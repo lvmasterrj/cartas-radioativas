5488e331e8cb8f744ae47ac8b7be49a86d5df6a9
lvmasterrj <lvmasterrj@gmail.com>
2022-02-24 16:53:00 -0300

    sempensar

--- sempensar/js/gerapdf.js
+++ sempensar/js/gerapdf.js
@@ -1,12 +1,68 @@
+var coordImpressao = {
+    corte: {
+        x: [17, 105, 193, 281],
+        y: [10, 73.5, 137, 200.5],
+    },
+    qtdCartas: 8,
+    tamanhoCarta: [63.5, 88],
+    margemCarta: 5,
+    tamanhoLogoVerso: [18, 18],
+    fonteCarta: 16,
+    fonteRodape: 11,
+    tamanhoFonte: 6, //mm
+    tamanhoFonteRodape: 2.2, //mm
+    correcaoRodape: 1.5,
+    sangria: 3,
+    1: [17, 10],
+    2: [17, 73.5],
+    3: [17, 137],
+    4: [105, 10],
+    5: [105, 73.5],
+    6: [105, 137],
+    7: [193, 10],
+    8: [193, 73.5],
+    9: [193, 137],
+};
+
+var cartas = {
+
+}
+
 const { jsPDF } = window.jspdf;
 
 $(document).ready(() => {
     criaPdf();
 });
 
+function desenhaLinhasDeCorte(doc) {
+    doc.setDrawColor(0);
+    doc.setLineWidth(0.1);
+
+    for (key in coordImpressao.corte.x) {
+        let x = coordImpressao.corte.x;
+        doc.line(x[key], 0, x[key], 5);
+        doc.line(x[key], 292, x[key], 297);
+    }
+    for (key in coordImpressao.corte.y) {
+        let y = coordImpressao.corte.y;
+        doc.line(0, y[key], 5, y[key]);
+        doc.line(205, y[key], 210, y[key]);
+    }
+}
+
 function criaPdf() {
-    const doc = new jsPDF();
-    doc.rect(20, 20, 10, 10);
+    const doc = new jsPDF({
+        orientation: "landscape"
+    });
+
+    desenhaLinhasDeCorte(doc);
+
+    //  for (key in coordImpressao) {
+    //      if (Object.hasOwnProperty.call(object, key)) {
+    //          const element = object[key];
+
+    //      }
+    //  }
 
     //doc.output("dataurlnewwindow", "cartas-radioativas.pdf"); // Exibe o pdf mas não salva
     window.open(doc.output('bloburl', "sem-pensar.pdf"), '_blank'); // Funciona mas o nome do arquivo fica ruim
