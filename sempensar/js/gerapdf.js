5209315e88f26c9aef90c2e3c6712341aebd8a87
lvmasterrj <lvmasterrj@gmail.com>
2022-02-24 16:36:26 -0300

    sempensar

--- sempensar/js/gerapdf.js
+++ sempensar/js/gerapdf.js
@@ -9,7 +9,7 @@ function criaPdf() {
     doc.rect(20, 20, 10, 10);
 
     //doc.output("dataurlnewwindow", "cartas-radioativas.pdf"); // Exibe o pdf mas não salva
-    window.open(doc.output('bloburl', "cartas-radioativas.pdf"), '_blank'); // Funciona mas o nome do arquivo fica ruim
+    window.open(doc.output('bloburl', "sem-pensar.pdf"), '_blank'); // Funciona mas o nome do arquivo fica ruim
     //  doc.save("cartas-radioativas"); // Salva com o nome correto,mas não faz preview
     console.log("OK")
 }
