8ad5e1a7ae7a2a9e5a8f90e768226808faf90e91
lvmasterrj <lvmasterrj@gmail.com>
2022-02-25 12:44:55 -0300

    SP

--- sempensar/js/gerapdf.js
+++ sempensar/js/gerapdf.js
@@ -90,7 +90,7 @@ function desenhaQuadroPontos(doc) {
     desenhaLinhasDeCorteCartas(doc);
 
     let margemCarta = 5, //mm
-        margemInterna = 1, //mm
+        margemInterna = 3, //mm
         tamanho = [12.5, 13], //mm
         rodadas = 12,
         jogadores = 8,
@@ -112,9 +112,9 @@ function desenhaQuadroPontos(doc) {
     //Cria os espaços para escrever os pontos de cada rodada
     let cont = [0, 0];
     for (let i = 0; i < (rodadas * jogadores); i++) {
-        let x = inicio[0] + 2 * margemInterna + cont[0] * tamanho[0];
-        let y = inicio[1] + 2 * margemInterna + cont[1] * tamanho[1];
-        let tamX = tamanho[0] - margemInterna;
+        let x = inicio[0] + margemInterna + cont[0] * tamanho[0];
+        let y = inicio[1] + margemInterna + cont[1] * tamanho[1];
+        let tamX = tamanho[0];
         let tamY = tamanho[1] - 2 * margemInterna;
 
         console.log("I = " + i + " | Cont = " + cont + " | X = " + x + " | Y = " + y + " | TamanhoX = " + tamX + " | TamanhoY = " + tamY);
