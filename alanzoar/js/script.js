9f83569679e35057c22429428637ddd3fd3446a6
lvmasterrj <lvmasterrj@gmail.com>
2022-03-11 12:18:41 -0300

    AR

--- alanzoar/js/script.js
+++ alanzoar/js/script.js
@@ -20,7 +20,7 @@ function pegaCartasBD() {
         .done(function(data) {
             impressao.perguntas = data;
             console.log(data);
-            // adicionaCategorias();
+            adicionaCategorias();
             $("#modal-aguarde").modal('hide');
         })
         .fail(function(e) {
@@ -43,14 +43,14 @@ function pegaCategoriasBD() {
 }
 
 
-// function adicionaCategorias() {
-//     for (key in dbCartas) {
-//         $("#botoes-categorias").append(
-//             `<input type="checkbox" class="btn-check btn-categoria" id="btn-${normaliza(key)}" autocomplete="off" value="${key}">
-// 				<label class="btn btn-outline-secondary" for="btn-${normaliza(key)}">${impressao.categorias[key]?'<img src="/imgs/icones/' + impressao.categorias[key] + '-preto.png" width="30" height="30" class="align-text-bottom me-2" imagem="' + impressao.categorias[key] + '">':''}${key} <span class="badge bg-dark qtd ms-1">${dbCartas[key].length}</span></label>`
-//         )
-//     }
-// }
+function adicionaCategorias() {
+    for (key in impressao.perguntas) {
+        $("#botoes-categorias").append(
+            `<input type="checkbox" class="btn-check btn-categoria" id="btn-${normaliza(key)}" autocomplete="off" value="${key}">
+				<label class="btn btn-outline-secondary" for="btn-${normaliza(key)}">${key} <span class="badge bg-dark qtd ms-1">${dbCartas[key].length}</span></label>`
+        )
+    }
+}
 
 // function normaliza(str) {
 //     return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
