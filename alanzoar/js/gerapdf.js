ae3535614ebe42d2726664583a056c9d938e5bb2
lvmasterrj <lvmasterrj@gmail.com>
2022-03-14 10:46:03 -0300

    Teste

--- alanzoar/js/gerapdf.js
+++ alanzoar/js/gerapdf.js
@@ -226,6 +226,7 @@ function montaFrenteCardsPerguntas(doc) {
         contCarta = 1,
         resto = impressao.imprimir.length % 5;
 
+
     for (let i = 0; i < coordImpressao.qtdCartas.total; i++) {
         let x = coordImpressao.corteCartas.x[carta[0]];
         let y = coordImpressao.corteCartas.y[carta[1]];
