b643c2c4e764b0ce76cf1197eeebae3e6e919af1
lvmasterrj <lvmasterrj@gmail.com>
2022-03-15 12:07:35 -0300

    AR

--- alanzoar/js/gerapdf.js
+++ alanzoar/js/gerapdf.js
@@ -299,12 +299,12 @@ function criaTexto(doc, x, y, margem, j, contCarta) {
     let textoNoTamanho = doc.setFontSize(11).splitTextToSize(texto, tamanhoMaxTexto);
     console.log(textoNoTamanho);
 
-    correcaoTexto = (textoNoTamanho.length - 1) * -2;
+    correcaoTexto = (textoNoTamanho.length - 1) * 2;
     console.log(correcaoTexto);
 
     //*** PEGAR O LENGHT DO ARRAY DO TEXTONOTAMANHO E CORRIGIR PARA CIMA DE ACORDO COM AS LINHAS
 
-    doc.text(textoNoTamanho, pontosTriangulo.x[2] + 2, pontosTriangulo.y[1] - 1);
+    doc.text(textoNoTamanho, pontosTriangulo.x[2] + 1, pontosTriangulo.y[1] - 1 - correcaoTexto);
 }
 
 function criaPdf() {
