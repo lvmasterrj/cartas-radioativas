30a02a5e6ade7f181b419390ca2c78ded6414115
lvmasterrj <lvmasterrj@gmail.com>
2022-06-13 10:55:07 -0300

    teste

--- js/gerapdf.js
+++ js/gerapdf.js
@@ -68,7 +68,7 @@ function emMM(tam, fontSize) {
 // Função que monta as linhas de corte
 function montaLinhasDeCorte(doc, cor = "b") {
     doc.setLineWidth(0.1);
-
+    console.log(cor);
     if (cor == "p") {
         doc.setDrawColor(255);
     } else {
